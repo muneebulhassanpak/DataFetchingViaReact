41b805e0946032819e111262e8c9d04a38621ab9
muneebulhassanpak <muneebulhassan121@gmail.com>
2023-03-26 19:51:26 +0500

    Self Fetching on Load using UseEffect

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,40 @@ import MoviesList from "./Components/MoviesList";
 
 const App = () => {
   const [movies, setMovies] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(null);
   const fetchMoviesHandler = () => {
+    setLoading(true);
+    setError(null);
     fetch("https://swapi.dev/api/films")
       .then((response) => {
         return response.json();
       })
+      .catch(() => {
+        setError(true);
+        throw new Error();
+      })
       .then((data) => {
+        setLoading(false);
         console.log(data.results);
         setMovies(data.results);
       });
   };
+  React.useEffect(() => {
+    fetchMoviesHandler();
+  }, []);
   return (
     <div className={styles["app-container"]}>
       <Header />
       <button onClick={fetchMoviesHandler} className={styles["fetch-button"]}>
         Fetch Movies
       </button>
-      <MoviesList movies={movies} />
+      {loading && error && <p>Error loading data</p>}
+      {loading && !error ? (
+        <p className={styles["loading-state"]}>Loading.........</p>
+      ) : (
+        <MoviesList movies={movies} />
+      )}
     </div>
   );
 };
